4713bb8c75279bd290608c89672f19df8f6cf4e9
urvishkapadiya <kapadiyaurvish16@gmail.com>
2024-08-19 23:37:01 -0400

    Feat: Add resource count

--- aws_resource_list_script.sh
+++ aws_resource_list_script.sh
@@ -2,7 +2,7 @@
 
 ###############################################################################
 # Author: Urvish Kapadiya
-# Version: v0.0.1
+# Version: v0.0.2
 
 # Below are the services that are supported by this script:
 # 1. EC2
@@ -20,10 +20,10 @@
 # 13. DynamoDB
 # 14. VPC
 # 15. EBS
-#
+
 # The script will prompt the user to enter the AWS region and the service for which the resources need to be listed.
 
-# Usage: ./aws_resource_list.sh  <aws_region> <aws_service>
+# Usage: ./aws_resource_list.sh <aws_region> <aws_service>
 # Example: ./aws_resource_list.sh us-east-1 ec2
 
 # functionalities:
@@ -31,13 +31,14 @@
 # 2. Log output to a file.
 # 3. Interactive mode for user input.
 # 4. Error handling and messages.
+# 5. Counting and listing details of resources.
 ###############################################################################
 
 # Log file setup
 log_file="aws_resource_list_$(date +%F_%T).log"
 exec > >(tee -i "$log_file") 2>&1
 
-# Function to list resources
+# Function to list and count resources
 list_resources() {
     aws_service=$1
     aws_region=$2
@@ -46,52 +47,69 @@ list_resources() {
 
     case $aws_service in
         ec2)
-            aws ec2 describe-instances --region $aws_region
+            resource_output=$(aws ec2 describe-instances --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"InstanceId"' | wc -l)
             ;;
         rds)
-            aws rds describe-db-instances --region $aws_region
+            resource_output=$(aws rds describe-db-instances --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"DBInstanceIdentifier"' | wc -l)
             ;;
         s3)
-            aws s3api list-buckets --region $aws_region | grep "BucketName" | awk -F'"' '{print $4}'
+            resource_output=$(aws s3api list-buckets --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"BucketName"' | wc -l)
             ;;
         cloudfront)
-            aws cloudfront list-distributions --region $aws_region
+            resource_output=$(aws cloudfront list-distributions --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"Id"' | wc -l)
             ;;
         vpc)
-            aws ec2 describe-vpcs --region $aws_region
+            resource_output=$(aws ec2 describe-vpcs --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"VpcId"' | wc -l)
             ;;
         iam)
-            aws iam list-users --region $aws_region | grep "UserName" | awk -F'"' '{print $4}'
+            resource_output=$(aws iam list-users --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"UserName"' | wc -l)
             ;;
         route53)
-            aws route53 list-hosted-zones --region $aws_region
+            resource_output=$(aws route53 list-hosted-zones --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"Name"' | wc -l)
             ;;
         cloudwatch)
-            aws cloudwatch describe-alarms --region $aws_region
+            resource_output=$(aws cloudwatch describe-alarms --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"AlarmName"' | wc -l)
             ;;
         cloudformation)
-            aws cloudformation describe-stacks --region $aws_region
+            resource_output=$(aws cloudformation describe-stacks --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"StackName"' | wc -l)
             ;;
         lambda)
-            aws lambda list-functions --region $aws_region | grep "FunctionName" | awk -F'"' '{print $4}'
+            resource_output=$(aws lambda list-functions --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"FunctionName"' | wc -l)
             ;;
         sns)
-            aws sns list-topics --region $aws_region
+            resource_output=$(aws sns list-topics --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"TopicArn"' | wc -l)
             ;;
         sqs)
-            aws sqs list-queues --region $aws_region
+            resource_output=$(aws sqs list-queues --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"QueueUrl"' | wc -l)
             ;;
         dynamodb)
-            aws dynamodb list-tables --region $aws_region
+            resource_output=$(aws dynamodb list-tables --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"TableName"' | wc -l)
             ;;
         ebs)
-            aws ec2 describe-volumes --region $aws_region
+            resource_output=$(aws ec2 describe-volumes --region $aws_region)
+            resource_count=$(echo "$resource_output" | grep -o '"VolumeId"' | wc -l)
             ;;
         *)
             echo "Invalid service: $aws_service"
             return 1
             ;;
     esac
+
+    echo "Total $aws_service resources found: $resource_count"
+    echo "$resource_output" | jq '.' # Pretty print the JSON output
 }
 
 # Function to handle multiple regions
